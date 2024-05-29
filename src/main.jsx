b10909b66bc62bde3ac3d89388bf59808f9336d1
Obi-Dan66 <124266422+Obi-Dan66@users.noreply.github.com>
2024-05-29 13:13:21 +0200

    adding imports to main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import './App.jsx'
 import './index.css'
+import './App.css'
+import './components/instructionsSharpTest.jsx';
+import './components/instructionsContrastTest.jsx';
+import './components/instructionsAstigmatismTest.jsx';
+import './components/instructionsEyeFieldTest.jsx';
+import './components/instructionsColorTest.jsx';
+import './components/sharptest.jsx';
+import './components/contrastTest.jsx';
+import './components/astigmatismTest.jsx';
+import './components/eyeFieldTest.jsx';
+import './components/colorTest.jsx';
+import './components/testSelection.jsx';
+import './infoPages/infoSharptest.jsx';
+import './infoPages/infoAstigmatismtest.jsx';
+import './infoPages/infoColortest.jsx';
+import './infoPages/infoEyeFieldtest.jsx';
+import './infoPages/infoContrasttest.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
