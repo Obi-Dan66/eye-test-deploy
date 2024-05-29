826e11ac4e2005f8e2e211441703b4bc07c74a0e
Obi-Dan66 <124266422+Obi-Dan66@users.noreply.github.com>
2024-05-29 13:28:10 +0200

    bug fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './App.jsx'
+import HomePage from './App.jsx'
 import './index.css'
-import './App.css'
-import './components/instructionsSharpTest.jsx';
-import './components/instructionsContrastTest.jsx';
-import './components/instructionsAstigmatismTest.jsx';
-import './components/instructionsEyeFieldTest.jsx';
-import './components/instructionsColorTest.jsx';
-import './components/sharptest.jsx';
-import './components/contrastTest.jsx';
-import './components/astigmatismTest.jsx';
-import './components/eyeFieldTest.jsx';
-import './components/colorTest.jsx';
-import './components/testSelection.jsx';
-import './infoPages/infoSharptest.jsx';
-import './infoPages/infoAstigmatismtest.jsx';
-import './infoPages/infoColortest.jsx';
-import './infoPages/infoEyeFieldtest.jsx';
-import './infoPages/infoContrasttest.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HomePage />
   </React.StrictMode>,
 )
