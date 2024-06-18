387b783616a7e761194ec3c2fcd02d0fb1044028
Obi-Dan66 <danielprochazka097@gmail.com>
2024-06-18 17:31:50 +0200

    rewriting to tailwind set up

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import "./index.css";
+import "./indexTailwind.css";
 import InstructionsSharpTest from "./instructionsPages/instructionsSharpTest.jsx";
 import InstructionsContrastTest from "./instructionsPages/instructionsContrastTest.jsx";
 import InstructionsAstigmatismTest from "./instructionsPages/instructionsAstigmatismTest.jsx";
