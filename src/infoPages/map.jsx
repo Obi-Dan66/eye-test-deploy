b65383e99d4d70ffb6bdca33a989ff01e696366f
Obi-Dan66 <danielprochazka097@gmail.com>
2024-07-01 15:56:54 +0200

    adding default map

--- src/infoPages/map.jsx
+++ src/infoPages/map.jsx
@@ -1,30 +1,50 @@
 import React, { useEffect } from "react";
+//import env from "./env"; //import for production use
 
 const Map = () => {
   useEffect(() => {
-    // Load the Google Maps JavaScript API script
-    const googleMapScript = document.createElement("script");
-    googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places`;
-    window.document.body.appendChild(googleMapScript);
+    let mapInstance; // Define mapInstance variable
 
-    googleMapScript.addEventListener("load", () => {
-      // Initialize the map
-      const map = new window.google.maps.Map(document.getElementById("map"), {
-        center: { lat: YOUR_LATITUDE, lng: YOUR_LONGITUDE },
-        zoom: 10,
+    const loadMapScript = () => {
+      return new Promise((resolve, reject) => {
+        const googleMapScript = document.createElement("script");
+        const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY || ""; // Access API key from environment variables
+        googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&callback=initMap&loading=async`;
+        googleMapScript.async = true; // Load the script asynchronously for better performance
+
+        window.initMap = () => {
+          mapInstance = new window.google.maps.Map(
+            document.getElementById("map"),
+            {
+              center: { lat: 0, lng: 0 }, // Default center at (0, 0)
+              zoom: 2, // Default zoom level
+            }
+          );
+        };
+
+        googleMapScript.addEventListener("load", () => {
+          resolve();
+        });
+
+        googleMapScript.addEventListener("error", (error) => {
+          reject(error);
+        });
+
+        window.document.body.appendChild(googleMapScript);
       });
+    };
 
-      // Add markers for store locations
-      // You can fetch store locations from your database and add markers here
-    });
+    loadMapScript().catch((error) => console.error(error)); // Handle any errors in the promise chain
 
     return () => {
       // Clean up to prevent memory leaks
-      window.document.body.removeChild(googleMapScript);
+      if (mapInstance) {
+        mapInstance = null; // Reset mapInstance
+      }
     };
   }, []);
 
-  return <div id="map" style={{ width: "100%", height: "400px" }}></div>;
+  return <div id="map" style={{ width: "100%", height: "600px" }}></div>;
 };
 
 export default Map;
