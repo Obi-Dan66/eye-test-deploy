fbcc74bf94ff98e5cc37dd8886af88330bbd019e
Obi-Dan66 <danielprochazka097@gmail.com>
2024-07-09 20:55:11 +0200

    cleanup -- working version

--- src/infoPages/googleLogin.jsx
+++ src/infoPages/googleLogin.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-// import env from "./env"; // Import for development only
 
 const GoogleLogin = () => {
   const navigate = useNavigate();
--- src/infoPages/map.jsx
+++ src/infoPages/map.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-// import env from "./env"; // Import for development only
 import axios from "axios";
 
 const Map = () => {
